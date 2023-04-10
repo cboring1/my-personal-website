e39a28d0fa9c267caf420c47b5299bb42df7e4dd
Cody Boring <codyboring94@gmail.com>
2023-04-09 17:43:57 -0700

    added education experience, an image, and a clickdown event. Working on making event a dropdown for work exp

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 const h2 = document.createElement("h2");
 h2.textContent = "This content added by JavaScript";
 document.querySelector("body").appendChild(h2);
+
+const work = document.getElementById('Work experience')
+work.addEventListener('click' , function() {
+    alert('I was clicked');
+})  /* working to add function to do dropdown on Work experience */
